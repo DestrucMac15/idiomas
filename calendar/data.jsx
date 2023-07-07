c88c900d7a7c346b9f119b22c24373308e092c93
Marco Antonio Medina Ortega <joungmac4@gmail.com>
2023-07-06 20:17:54 -0600

    first commit

---
+++ calendar/data.jsx
@@ -0,0 +1,14 @@
+export const eventsList = [
+  '2023:10:29',
+];
+
+export const tempEvents = [
+  {
+    "id": "1",
+    "title": "Día de los muertos",
+    "description": "Las festividades anuales del día de los muertos se han vuelto muy populares en México entre los visitantes internacionales. El desfile principal en el centro de la ciudad es una producción espectacular.",
+    "date": "2023:10:29",
+    "imgUrl": 'dia-de-muertos.webp',
+  }
+];
+
